feb7113ba076022de3133f513cf034aa8a9d22ba
Yi Fei Huang <hyf97ca@gmail.com>
2023-01-25 20:46:30 -0500

    Basic collisions kind of working + 25 asteroids

--- astroid.py
+++ astroid.py
@@ -13,8 +13,8 @@ import sys, math
 BLACK = (0, 0, 0, 255)
 WHITE = (255, 255, 255, 255)
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 1280
+HEIGHT = 720
 
 if __name__ == "__main__":
     pygame.init()
@@ -71,51 +71,95 @@ class PhysicsSprite(Sprite):
 
     def process_single_collision(sprite, collided_sprite):
         #find collision point
-        collision_mask_point = pygame.sprite.collide_mask(sprite, collided_sprite)
-        if collision_mask_point is None:
-            return
 
-        pygame.draw.circle(sprite.image, (255, 0, 0), collision_mask_point, 5)
-        
-        collision_mask_rect = sprite.mask.to_surface().get_bounding_rect()
-        #collision_mask_rect = sprite.image.get_bounding_rect()
-        collision_point = Vector2(collision_mask_point) - Vector2(collision_mask_rect.center)
-
-        #do projection of self velocity in collision point direction
-        collision_point_normal = collision_point.rotate(180)
-        #use reflect to bounce vector off of rotated collision point
-        if sprite.vector and sprite.vector.magnitude() != 0:
-            pygame.math.Vector2.reflect_ip(sprite.vector, collision_point_normal)
-            #sprite.vector += sprite.vector.reflect(collision_point_normal)
-            #pygame.math.Vector2.rotate_ip(sprite.vector, 180)
-        #back off a bit
-        
-        #collision_mask_point = pygame.sprite.collide_mask(sprite, collided_sprite)
-        touching = False
-        #touching = collision_mask_point is not None
-        while touching:
-            #collision_point = Vector2(collision_mask_point) - Vector2(collision_mask_rect.center)
-            pygame.math.Vector2.normalize_ip(collision_point)
-            pygame.math.Vector2.rotate_ip(collision_point, 180)
-            if collision_point[0] > 0:
-                sprite.x += math.ceil(collision_point[0])*(sprite.vector.magnitude() + 1)
-            else:
-                sprite.x += math.floor(collision_point[1])*(sprite.vector.magnitude() + 1)
-            
-            if collision_point[1] > 0:
-                sprite.y += math.ceil(collision_point[0])*(sprite.vector.magnitude() + 1)
-            else:
-                sprite.y += math.floor(collision_point[1])*(sprite.vector.magnitude() + 1)
-
-            #update mask positions
-            image_center = sprite.image.get_rect(center = (sprite.x, sprite.y)).center
-            sprite.rect = sprite.image.get_rect(center = image_center)
-            sprite.mask = pygame.mask.from_surface(sprite.image, 240)
-
+        if False:#hasattr(sprite, 'radius'):
+            collision_point = Vector2(sprite.x, sprite.y) - Vector2(collided_sprite.x, collided_sprite.y)
+            collision_point.normalize_ip()
+            collision_point *= sprite.radius
+            collision_mask_point = Vector2(sprite.image) + collision_point
+            pygame.draw.circle(sprite.image, (255, 0, 0), collision_point, 5)
+            collision_point_normal = collision_point.rotate(180)
+            #use reflect to bounce vector off of rotated collision point
+            if sprite.vector and sprite.vector.magnitude() != 0:
+                pygame.math.Vector2.reflect_ip(sprite.vector, collision_point_normal)
+            # sprite_position = Vector2(sprite.x, sprite.y)
+            # collided_sprite_position = Vector2(collided_sprite.x, collided_sprite.y)
+            # distance = sprite_position.distance_to(collided_sprite_position)
+            # return distance < sprite.radius + collided_sprite.radius
+        else:
             collision_mask_point = pygame.sprite.collide_mask(sprite, collided_sprite)
+            if collision_mask_point is None:
+                return
+
+            count = 1
+            touching = True
+            touching_collision_mask_point = Vector2(collision_mask_point)
+            #touching = collision_mask_point is not None
+            while touching:
+                collision_mask_rect = sprite.mask.to_surface().get_bounding_rect()
+                touching_collision_point = Vector2(collision_mask_rect.center) - Vector2(touching_collision_mask_point)
+                pygame.math.Vector2.normalize_ip(touching_collision_point)
+                #pygame.math.Vector2.rotate_ip(touching_collision_point, 180)
+                widest_point = count#max(collision_mask_rect.width, collision_mask_rect.height)
+
+                if touching_collision_point[0] > 0:
+                    #sprite.x += math.ceil(touching_collision_point[0]) * widest_point
+                    collided_sprite.x -= math.ceil(touching_collision_point[0]) * widest_point
+                else:
+                    sprite.x += math.floor(touching_collision_point[0]) * widest_point
+                    collided_sprite.x -= math.floor(touching_collision_point[0]) * widest_point
+                
+                if touching_collision_point[1] > 0:
+                    #sprite.y -= math.ceil(touching_collision_point[1]) * widest_point
+                    collided_sprite.y -= math.ceil(touching_collision_point[1]) * widest_point
+                else:
+                    #sprite.y += math.floor(touching_collision_point[1]) * widest_point
+                    collided_sprite.y -= math.floor(touching_collision_point[1]) * widest_point
+
+                collided_sprite.rect.centerx = collided_sprite.x
+                collided_sprite.rect.centery = collided_sprite.y
+                #sprite.rect.centerx = sprite.x
+                #sprite.rect.centery = sprite.y
+
+                # #update mask positions
+                #image_center = sprite.image.get_rect(center = (sprite.x, sprite.y)).center
+                #sprite.rect = sprite.image.get_rect(center = image_center)
+                #sprite.mask = pygame.mask.from_surface(sprite.image, 240)
+                #update mask positions
+                image_center = collided_sprite.image.get_rect(center = (collided_sprite.x, collided_sprite.y)).center
+                collided_sprite.rect = collided_sprite.image.get_rect(center = image_center)
+                collided_sprite.mask = pygame.mask.from_surface(collided_sprite.image, 240)
+
+                count *= 2
+                touching_collision_mask_point = pygame.sprite.collide_mask(sprite, collided_sprite)
+                if count > 100:
+                    sprite.kill()
+                touching = False#touching_collision_mask_point is not None
+
+            pygame.draw.circle(sprite.image, (255, 0, 0), collision_mask_point, 10)
+
+            #pygame.draw.circle(sprite.image, (0,255,0), sprite.mask.to_surface().get_bounding_rect().center, 5)
+            
             collision_mask_rect = sprite.mask.to_surface().get_bounding_rect()
             #collision_mask_rect = sprite.image.get_bounding_rect()
-            touching = collision_mask_point is not None
+            collision_point = Vector2(collision_mask_rect.center) - Vector2(collision_mask_point)
+
+            #do projection of self velocity in collision point direction
+            collision_point_normal = collision_point#collision_point.rotate(180)
+
+            #finally, cancel out velocity change from update()
+            sprite.x -= sprite.vector[0]
+            sprite.y -= sprite.vector[1]
+            sprite.rect.centerx = sprite.x
+            sprite.rect.centery = sprite.y
+
+            #use reflect to bounce vector off of rotated collision point
+            if sprite.vector and sprite.vector.magnitude() != 0:
+                pygame.math.Vector2.reflect_ip(sprite.vector, collision_point_normal)
+                #sprite.vector += sprite.vector.reflect(collision_point_normal)
+                #pygame.math.Vector2.rotate_ip(sprite.vector, 180)
+
+            #collision_mask_point = pygame.sprite.collide_mask(sprite, collided_sprite)
 
     def on_collision(self, collision_list: List[Sprite]) -> None:
         for sprite in collision_list:
@@ -144,6 +188,7 @@ class SpaceShip(PhysicsSprite):
             self.THRUST_STRENGTH = 0.3 # pixels/frame
             self.ROTATION_STRENGTH = 4 # degrees
             self.INERTIAL_DAMPENING = 0.99 # float from 0-1
+            self.radius = 10
 
         def draw_thrust(self):
             if self.thrust_on:
@@ -211,6 +256,7 @@ class Asteroid(PhysicsSprite):
         self.mask = pygame.mask.from_surface(self.image, 240)
         self.size = size
         self.initalRot = initialRot
+        self.radius = 80
 
     def update(self, *args, **kwargs) -> None:
         if self.size <= 0:
@@ -264,14 +310,14 @@ if __name__ == "__main__":
     spaceship = SpaceShip(WIDTH/2, HEIGHT/2, 0, 0)
     all_sprites.add(spaceship)
 
-    for x in range(5):
+    for x in range(25):
         asteroid = Asteroid(
             random.randint(0, WIDTH),
             random.randint(0, HEIGHT),
             random.uniform(0.3, 1) * random.choice([-1, 1]),
             random.uniform(0.3, 1) * random.choice([-1, 1]),
             random.randint(5, 15) * random.choice([-1, 1]),
-            3
+            random.randint(1, 3)
         )
         print(asteroid.x, asteroid.y, asteroid.vector.x, asteroid.vector.y, asteroid.initalRot)
         all_sprites.add(asteroid)
@@ -283,7 +329,7 @@ if __name__ == "__main__":
                 running = False
 
         all_sprites.update(*all_sprites)
-        hit_list = pygame.sprite.groupcollide( all_sprites, all_sprites, False, False)
+        hit_list = pygame.sprite.groupcollide( all_sprites, all_sprites, False, False, pygame.sprite.collide_mask)
         for sprite in hit_list.keys():
             sprite.on_collision( hit_list[ sprite ] )
 
